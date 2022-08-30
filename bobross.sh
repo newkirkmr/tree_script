80ba50ee088522318743197b1cbcbe38123d6814
newkirkmr <94079902+newkirkmr@users.noreply.github.com>
2022-08-30 13:32:39 -0700

    Update bobross.sh

--- bobross.sh
+++ bobross.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 apt-get update
 apt-get install tree jp2a -y
-mkdir ~/ptree && cd ~/ptree
-wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross_lib -P .
-wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross_bin -P .
-wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross.jpg -P .
-wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/ptree.sh -P .
-chmod +x bobross_lib
-chmod +x bobross_bin
-chmod +x ptree.sh
-mv bobross_lib /usr/local/lib/bobross_lib
-mv bobross_bin /usr/local/bin/bobross_bin
-mv bobross.jpg /usr/local/lib/bobross.jpg
-mv ptree.sh /usr/local/bin/ptree.sh
-mv /usr/local/lib/bobross_lib /usr/local/lib/bobross
-mv /usr/local/bin/bobross_bin /usr/local/bin/bobross
-mv /usr/local/bin/ptree.sh /usr/local/bin/ptree
-cd ~
-rm -R ptree bobross.sh 
+
+wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross_lib -P /usr/local/lib
+wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross -P /usr/local/bin
+wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross.jpg -P /usr/local/lib
+wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/ptree -P /usr/local/lib
+chmod +x /usr/local/bin/bobross
+
+chmod +x /usr/local/lib/ptree
+
+
+
+
+
+
+
+
+
 
 bobross
