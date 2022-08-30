90a00efaf5865a3711eac98527f3da31c36b35bd
newkirkmr <94079902+newkirkmr@users.noreply.github.com>
2022-08-30 05:44:09 -0700

    Update bobross.sh

--- bobross.sh
+++ bobross.sh
@@ -8,7 +8,7 @@ wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross.jpg -P
 wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/ptree.sh -P .
 chmod +x bobross_lib
 chmod +x bobross_bin
-chmod -x ptree.sh
+chmod +x ptree.sh
 mv bobross_lib /usr/local/lib/bobross_lib
 mv bobross_bin /usr/local/bin/bobross_bin
 mv bobross.jpg /usr/local/lib/bobross.jpg
@@ -18,4 +18,5 @@ mv /usr/local/bin/bobross_bin /usr/local/bin/bobross
 mv /usr/local/bin/ptree.sh /usr/local/bin/ptree
 cd ~
 rm -R ptree bobross.sh 
+
 bobross
