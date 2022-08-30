0c1a8e20d5d00e2a844cb5f6ab1fc18350ca9afa
newkirkmr <94079902+newkirkmr@users.noreply.github.com>
2022-08-30 05:34:48 -0700

    Update bobross.sh

--- bobross.sh
+++ bobross.sh
@@ -4,7 +4,7 @@ apt-get install tree jp2a -y
 mkdir ~/ptree && cd ~/ptree
 wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross_lib -P .
 wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross_bin -P .
-wget https://github.com/newkirkmr/tree_script/blob/126ab29088b99ebd4d08684dfc072dc8c4024f7d/bobross.jpg -P .
+wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/bobross.jpg -P .
 wget https://raw.githubusercontent.com/newkirkmr/tree_script/main/ptree.sh -P .
 chmod +x bobross_lib
 chmod +x bobross_bin
